0f4c9a3eccb13692a96a3924752d60c35d571f25
Sung Paik <sung.paik@codeimmersives.com>
2020-04-09 16:12:00 -0400

    included margin-left in the main app and vertical aligned the text within the like and dislike section

--- src/components/Sidebars/Sidebar/Sidebar.js
+++ src/components/Sidebars/Sidebar/Sidebar.js
@@ -3,7 +3,7 @@ import React from 'react';
 const sidebar = (props) => {
     return (
         <li className="sidebar">
-                {props.children}
+            {props.children}
         </li>
     )
 }
