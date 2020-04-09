a48a41cbc38c33a084dded9caf7af9df7d3de8f7
Sung Paik <sung.paik@codeimmersives.com>
2020-04-09 11:43:57 -0400

    included images of animals and setup the animal component to render each animal

---
+++ src/components/Animals.js
@@ -0,0 +1,123 @@
+import React, { Component } from 'react';
+
+let animals = [
+    { 
+        image: './images/trex/jpg',
+        type: 'dinosaur',
+        name: 'Tyrannosaurus',
+        description: 'Tyrannosaurus is a genus of coelurosaurian theropod dinosaur. The species Tyrannosaurus rex (rex meaning "king" in Latin), often called T. rex or colloquially T-Rex, is one of the most well-represented of the large theropods.',
+        animalId: '1'
+    },
+    { 
+        image: './images/triceri.png',
+        type: 'dinosaur',
+        name: 'Triceratops',
+        description: 'Triceratops is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period, about 68 million years ago (mya) in what is now North America.',
+        animalId: '2'
+    },
+    { 
+        image: './images/sauropod.jpg',
+        type: 'dinosaur',
+        name: 'Sauropoda',
+        description: 'Sauropods were herbivorous (plant-eating), usually quite long-necked quadrupeds (four-legged), often with spatulate (spatula-shaped: broad at the tip, narrow at the neck) teeth. They had tiny heads, massive bodies, and most had long tails.',
+        animalId: '3'
+    },
+    { 
+        image: './images/velo.jpeg',
+        type: 'dinosaur',
+        name: 'Velociraptor',
+        description: 'Velociraptor (/vɪˈlɒsɪræptər/; meaning "swift seizer" in Latin) is a genus of dromaeosaurid theropod dinosaur that lived approximately 75 to 71 million years ago during the latter part of the Cretaceous Period. Two species are currently recognized, although others have been assigned in the past.',
+        animalId: '4'
+    },
+    { 
+        image: './images/greatwhite.jpg',
+        type: 'fish',
+        name: 'Great White Shark',
+        description: 'The great white shark (Carcharodon carcharias), also known as the great white, white shark or "white pointer", is a species of large mackerel shark which can be found in the coastal surface waters of all the major oceans.',
+        animalId: '5'
+    },
+    { 
+        image: './images/hammerhead.jpg',
+        type: 'fish',
+        name: 'Hammerhead Shark',
+        description: 'The hammerhead sharks are a group of sharks that form the family Sphyrnidae, so named for the unusual and distinctive structure of their heads, which are flattened and laterally extended into a "hammer" shape called a cephalofoil.',
+        animalId: '6'
+    },
+    { 
+        image: './images/whaleshark.jpg',
+        type: 'fish',
+        name: 'Whale Shark',
+        description: 'Whale sharks are the biggest fish in the sea. A whale shark can be as long as a school bus and weigh as much as 50,000 pounds. Whale sharks are one of only three known shark species that filter feed, as baleen whales do.',
+        animalId: '7'
+    },
+    { 
+        image: './images/dolphin.png',
+        type: 'fish',
+        name: 'Dolphin',
+        description: 'Dolphin is a common name of aquatic mammals within the infraorder Cetacea. The term dolphin usually refers to the extant families Delphinidae (the oceanic dolphins), Platanistidae (the Indian river dolphins), Iniidae (the New World river dolphins), and Pontoporiidae (the brackish dolphins), and the extinct Lipotidae (baiji or Chinese river dolphin). There are 40 extant species named as dolphins.',
+        animalId: '8'
+    },
+    { 
+        image: './images/tiger.jpg',
+        type: 'cat',
+        name: 'Bengal Tiger',
+        description: 'The Bengal tiger is a Panthera tigris tigris population native to the Indian subcontinent. It is threatened by poaching, loss, and fragmentation of habitat, and was estimated at comprising fewer than 2,500 individuals by 2011.',
+        animalId: '9'
+    },
+    { 
+        image: './images/panther.jpg',
+        type: 'cat',
+        name: 'Black Panther',
+        description: 'A black panther is the melanistic colour variant of any Panthera, particularly of the leopard (P. pardus) in Asia and Africa, and the jaguar (P. onca) in the Americas.',
+        animalId: '10'
+    },
+    { 
+        image: './images/ocelot.jpg',
+        type: 'cat',
+        name: 'Ocelot',
+        description: 'The ocelot (/ˈɒsəlɒt/; Leopardus pardalis) is a small wild cat native to the southwestern United States, Mexico, and Central and South America. This medium-sized cat is characterized by solid black spots and streaks on its coat, round ears, and white neck and undersides.',
+        animalId: '11'
+    },
+    { 
+        image: './images/cat.jpg',
+        type: 'cat',
+        name: 'House Cat',
+        description: 'Evil creatures',
+        animalId: '12'
+    },
+]
+
+class Animals extends Component {
+    render() {
+        let animal = animals.map(({image, type, name, description, animalId}) => {
+            return (
+                <div className="ui card" key={animalId}>
+                    <div className="image">
+                        <img src={image} alt="..." />
+                    </div>
+                    <div className="content">
+                        <a className="header">{name}</a>
+                        <div className="meta">
+                        <span className="type">{type}</span>
+                        </div>
+                        <div className="description">
+                            {description}
+                        </div>
+                    </div>
+                    <div className="extra content">
+                        <button>Like</button>
+                        <button>Dislike</button>
+                        <button>Discard</button>
+                    </div>
+                </div>
+            )
+        })
+        return (
+            <div>
+                {animal}
+            </div>
+        )
+    }
+};
+
+export default Animals;
---
+++ src/components/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Animals from './Blogs';
+
+const App = () => (
+        <Animals />
+);
+
+export default App;
